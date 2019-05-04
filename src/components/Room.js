8b7b63c65e7dc54543980e99eb06c27ac9a38a70
Viktoriia Kuznietsova <viktoriia.kuznietsova@x-akseli.fi>
2019-05-05 00:38:06 +0300

    Add room image slider
    Using arrows instead of dots for changing pictures
    because dots would be too small to click.

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Room.css";
+import imageNotFound from "../images/image-note-found.jpg";
 
 const Room = props => {
   const roomInformation = props.roomInformation(props.availableRoom.type);
+  const imagesLength = roomInformation.imageURLs.length;
+  const slideShowAvailable = imagesLength > 1;
+
+  const [currentImage, setCurrentImage] = useState(
+    imagesLength === 0 ? imageNotFound : roomInformation.imageURLs[0]
+  );
+
+  const onNextImage = currentImage => {
+    let newIndex = roomInformation.imageURLs.indexOf(currentImage) + 1;
+    newIndex === imagesLength
+      ? setCurrentImage(roomInformation.imageURLs[0])
+      : setCurrentImage(roomInformation.imageURLs[newIndex]);
+  };
+
+  const onPrevImage = currentImage => {
+    let newIndex = roomInformation.imageURLs.indexOf(currentImage) - 1;
+    newIndex === -1
+      ? setCurrentImage(roomInformation.imageURLs[imagesLength - 1])
+      : setCurrentImage(roomInformation.imageURLs[newIndex]);
+  };
 
   const minPriceInEuroCents = rates => {
     let formatter = new Intl.NumberFormat("fi-FI", {
@@ -17,9 +38,27 @@ const Room = props => {
 
   return (
     <div className="room-row">
-      <div>
-        {/* TODO: add slider for image gallery */}
-        <img className="room-image" alt="" src={roomInformation.imageURLs[0]} />
+      <div className="room-slideshow-container">
+        <div className="room-slides">
+          <img className="room-image" alt="" src={currentImage} />
+        </div>
+
+        {slideShowAvailable && (
+          <div>
+            <button
+              className="prev-room"
+              onClick={() => onPrevImage(currentImage)}
+            >
+              &#10094;
+            </button>
+            <button
+              className="next-room"
+              onClick={() => onNextImage(currentImage)}
+            >
+              &#10095;
+            </button>
+          </div>
+        )}
       </div>
       <div className="room-info">
         <div className="room-name">{roomInformation.title}</div>
